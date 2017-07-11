bc722f696b7b5f56c819546d9b93b8619d7c1349
linky00 <linky00@plotholestudios.com>
2017-07-11 11:16:09 +0100

    Main loop established, prints outcome

--- economy.py
+++ economy.py
@@ -1,5 +1,8 @@
+import random
+
 class Person:
-    def __init__ (self, startmoney):
+    def __init__ (self, id, startmoney):
+        self.id = id
         self.money = startmoney
 
     def give_money (self, amount):
@@ -8,11 +11,36 @@ class Person:
     def recieve_money (self, amount):
         self.money += amount
 
+    def you_need_money_to_live (self):
+        return self.money < 0
+
 def transfer_money (amount, giver, receiver):
     giver.give_money(amount)
     receiver.recieve_money(amount)
 
-people = []
+no_of_people = 100
+startmoney = 10
+amount = 1
+length = 1000
 
-for x in range(10):
-    people.append(Person(10))
+people = []
+for i in range(no_of_people):
+    people.append(Person(i, startmoney))
+
+for i in range(length):
+    index_list = list(range(len(people)))
+    for person in people:
+        other_person = person
+        while other_person == person:
+            other_person = people[random.choice(index_list)]
+        transfer_money(amount, person, other_person)
+
+    for person in people:
+        if person.you_need_money_to_live():
+            people.remove(person)
+
+    if i % 100 == 0 or i == length - 1:
+        print("Round " + str(i) + " outcome:")
+        for person in people:
+            print("ID " + str(person.id) + " has " + str(person.money))
+        print("---")
