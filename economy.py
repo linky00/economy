db9f1115ace98d2f9db06ede78f0956cb0cc4210
linky00 <linky00@plotholestudios.com>
2017-07-17 21:11:12 +0100

    Added graphing, removed murder

--- economy.py
+++ economy.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 class Person:
     def __init__ (self, id, startmoney):
@@ -10,9 +11,6 @@ class Person:
 
     def recieve_money (self, amount):
         self.money += amount
-
-    def you_need_money_to_live (self):
-        return self.money < 0
 
 def transfer_money (amount, giver, receiver):
     giver.give_money(amount)
@@ -30,17 +28,20 @@ for i in range(no_of_people):
 for i in range(length):
     index_list = list(range(len(people)))
     for person in people:
-        other_person = person
-        while other_person == person:
-            other_person = people[random.choice(index_list)]
-        transfer_money(amount, person, other_person)
-
-    for person in people:
-        if person.you_need_money_to_live():
-            people.remove(person)
+        if person.money > 0:
+            other_person = person
+            while other_person == person:
+                other_person = people[random.choice(index_list)]
+            transfer_money(amount, person, other_person)
 
     if i % 100 == 0 or i == length - 1:
         print("Round " + str(i) + " outcome:")
         for person in people:
             print("ID " + str(person.id) + " has " + str(person.money))
         print("---")
+
+x = []
+for person in people:
+    x.append(person.money)
+plt.hist(x)
+plt.show()
