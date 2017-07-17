d0a8b6fe66b7c7d95821bff06515e6fac4b91888
linky00 <linky00@plotholestudios.com>
2017-07-17 21:53:27 +0100

    Now we using bar charts...

--- economy.py
+++ economy.py
@@ -1,6 +1,7 @@
 import random
 import matplotlib.pyplot as plt
 
+# person class
 class Person:
     def __init__ (self, id, startmoney):
         self.id = id
@@ -12,19 +13,23 @@ class Person:
     def recieve_money (self, amount):
         self.money += amount
 
+# function to make things easier
 def transfer_money (amount, giver, receiver):
     giver.give_money(amount)
     receiver.recieve_money(amount)
 
-no_of_people = 100
+# various variables
+no_of_people = 1000
 startmoney = 10
 amount = 1
-length = 1000
+length = 10000
 
+# people setup
 people = []
 for i in range(no_of_people):
     people.append(Person(i, startmoney))
 
+# main loop
 for i in range(length):
     index_list = list(range(len(people)))
     for person in people:
@@ -34,14 +39,18 @@ for i in range(length):
                 other_person = people[random.choice(index_list)]
             transfer_money(amount, person, other_person)
 
+    # printing stuff
     if i % 100 == 0 or i == length - 1:
         print("Round " + str(i) + " outcome:")
         for person in people:
             print("ID " + str(person.id) + " has " + str(person.money))
         print("---")
 
-x = []
+# graphing
+people_money = []
 for person in people:
-    x.append(person.money)
-plt.hist(x)
+    people_money.append(person.money)
+people_money.sort()
+people_money.reverse()
+plt.bar(range(len(people)), people_money)
 plt.show()
